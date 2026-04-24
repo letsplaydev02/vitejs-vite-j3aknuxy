e72276671849ad3dc220feabb69f83e9ae368d10
letsplaydev02 <git@stackblitz.com>
2026-04-24 11:00:26 +0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'Arial' }}>
+      <h1>Secure Message</h1>
+      <p>Сайт работает онлайн.</p>
+    </div>
+  );
+}
